524def352002bb30dbf6af4c946ec7fccae59203
abelagaba <77072924+abelagaba@users.noreply.github.com>
2022-06-27 13:18:09 +0300

    iniial commit

---
+++ app/src/main/java/com/agaba/firebaseapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.agaba.firebaseapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
